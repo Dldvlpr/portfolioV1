1070866ed7a62abe9979835c769873c54f69f10b
dldvlpr <dormoy.dev@gmail.com>
2023-08-13 00:32:29 +0200

    build home page with css and animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.scss'
 import Layout from "./components/Layout/index.jsx";
 import {Route, Routes} from "react-router-dom";
+import Home from "./components/Home/home.jsx";
 
 function App() {
 
   return (
     <>
         <Routes>
-            <Route path="/" element={<Layout />}/>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Home />} />
+            </Route>
         </Routes>
     </>
   )
